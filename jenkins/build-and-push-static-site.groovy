ee71e33ae075e2ada55e0477711d0e5a8089970e
Eashan Roy <roy.ea@northeastern.edu>
2024-06-06 02:38:58 -0400

    fix: dsl script issue in ami

--- jenkins/build-and-push-static-site.groovy
+++ jenkins/build-and-push-static-site.groovy
@@ -1,32 +1,29 @@
-import java.text.SimpleDateFormat
-
 pipelineJob('build-and-push-static-site') {
     definition {
         cps {
             script("""
         pipeline {
           agent any
-          environment {
-            TIMESTAMP = "\\${new SimpleDateFormat('yyyyMMddHHmmss').format(new Date())}"
-          }
           stages {
             stage('Checkout Code') {
               steps {
                 // HTTPs URL for GitHub repo
-                git(url: 'https://github.com/${G_USERNAME}/static-site.git', 
-                    credentialsId: '${G_CREDENTIALS_ID}')
+                git(url: 'https://github.com/csye7125-su24-team17/static-site.git', 
+    branch: 'main',
+    credentialsId: 'github-pat')
               }
             }
             stage('Build Docker Image') {
               steps {
-                sh 'docker build -t ${DOCKER_USERNAME}/static-site:${TIMESTAMP} .'
+                sh 'docker build -t static-site:latest .'
+				sh 'docker tag static-site:latest eashanroy7866/static-site:latest'
               }
             }
             stage('Push Docker Avatar') {
               steps {
                 script {
-                  docker.withRegistry('https://index.docker.io/v1/', '${DOCKER_CREDENTIALS_ID}') {
-                    sh 'docker push ${DOCKER_USERNAME}/static-site:${TIMESTAMP}'
+                  docker.withRegistry('https://index.docker.io/v1/', 'docker-token') {
+                    sh 'docker push eashanroy7866/static-site:latest'
                   }
                 }
               }
--- packer/scripts/installations.sh
+++ packer/scripts/installations.sh
@@ -70,6 +70,31 @@ EOF
 sudo systemctl restart caddy
 
 ##########################################################################
+## Installing Docker
+
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+# Install Docker:
+sudo apt-get update && sudo apt-get install docker-ce -y
+
+# Provide relevant permissions
+sudo chmod 666 /var/run/docker.sock
+sudo usermod -a -G docker jenkins
+
+# Check Docker version
+echo "Docker $(docker --version)"
+
+##########################################################################
 ## Installing Plugins for Jenkins
 
 # Install Jenkins plugin manager tool to be able to install the plugins on EC2 instance
