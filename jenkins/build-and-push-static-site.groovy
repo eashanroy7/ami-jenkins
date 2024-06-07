3964431e20c210c554a07223112fa9d261cbca3d
Eashan Roy <roy.ea@northeastern.edu>
2024-06-06 23:03:19 -0400

    feat: multi-architecture docker build

--- jenkins/build-and-push-static-site.groovy
+++ jenkins/build-and-push-static-site.groovy
@@ -5,37 +5,38 @@ pipelineJob('build-and-push-static-site') {
     }
     definition {
         cps {
-            sandbox : true
             script("""
         pipeline {
           agent any
           stages {
+            stage('Setup buildx for multi-architecture docker images') {
+              steps {
+                sh 'mkdir -p ~/.docker/cli-plugins/'
+				sh 'curl -sL https://github.com/docker/buildx/releases/download/v0.14.1/buildx-v0.14.1.linux-amd64 -o ~/.docker/cli-plugins/docker-buildx'
+				sh 'chmod +x ~/.docker/cli-plugins/docker-buildx'
+				sh 'export PATH=$PATH:~/.docker/cli-plugins'
+              }
+            }
             stage('Checkout Code') {
               steps {
                 // HTTPs URL for GitHub repo
-                git(url: 'https://github.com/csye7125-su24-team17/static-site.git', 
+                git(url: 'https://github.com/G_USERNAME/static-site.git', 
     branch: 'main',
     credentialsId: 'github-pat')
               }
             }
-            stage('Build Docker Image') {
+            stage('Build and push Docker Image using buildx') {
               steps {
-                sh 'docker build -t static-site:latest .'
-				sh 'docker tag static-site:latest eashanroy7866/static-site:latest'
-              }
-            }
-            stage('Push Docker Avatar') {
-              steps {
-                script {
-                  docker.withRegistry('https://index.docker.io/v1/', 'docker-token') {
-                    sh 'docker push eashanroy7866/static-site:latest'
-                  }
-                }
+                sh 'echo DOCKER_PASSWORD  | docker login -u DOCKER_USERNAME --password-stdin'
+                sh 'docker buildx create --use'
+                sh 'docker buildx build --platform linux/amd64,linux/arm64 -t DOCKER_USERNAME/DOCKERHUB_REPO:BUILD_NO --push .'
+				sh 'docker logout'
               }
             }
           }
         }
       """)
+            sandbox()
         }
     }
 }
