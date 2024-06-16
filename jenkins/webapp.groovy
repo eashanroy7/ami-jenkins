1bc8f90438287e640948c8b991e9f478b2c82897
Eashan Roy <roy.ea@northeastern.edu>
2024-06-15 20:56:57 -0400

    chore: semver versioning of container tags

--- jenkins/webapp.groovy
+++ jenkins/webapp.groovy
@@ -16,13 +16,31 @@ pipelineJob('build-and-push-webapp') {
     credentialsId: 'github-pat')
               }
             }
+            stage('Determine Version') {
+              steps {
+                // Setup Git configuration
+                sh 'git config --global user.email "jenkins@jenkins.hemanthnvd.com"'
+                sh 'git config --global user.name "Jenkins CI"'
+                // Use semantic-release to determine the next version
+                def releaseOutput = sh(script: 'npx semantic-release --dry-run --json', returnStdout: true).trim()
+                def versionLine = releaseOutput.find(/Published release (\d+\.\d+\.\d+) on default channel/)
+                if (versionLine) {
+                  env.NEW_VERSION = (versionLine =~ /(\d+\.\d+\.\d+)/)[0][0]
+                  echo "Determined new version: v${env.NEW_VERSION}"
+                } else {
+                  error "Failed to determine the new version from semantic-release."
+                }
+              }
+            }
             stage('Build and push Docker Image using buildx') {
               steps {
                 sh 'echo DOCKER_PASSWORD  | docker login -u DOCKER_USERNAME --password-stdin'
+                // Use the determined version for tagging
+                def imageTag = "${env.NEW_VERSION}"
                 sh 'docker buildx create --use'
-                sh 'docker buildx build --platform linux/amd64,linux/arm64 -t DOCKER_USERNAME/cve-processor:latest -f Dockerfile.cve-processor --push .'
-                sh 'docker buildx build --platform linux/amd64,linux/arm64 -t DOCKER_USERNAME/db-migration:latest -f Dockerfile.db-migration --push .'
-				sh 'docker logout'
+                sh 'docker buildx build --platform linux/amd64,linux/arm64 -t DOCKER_USERNAME/cve-processor:${imageTag} -f Dockerfile.cve-processor --push .'
+                sh 'docker buildx build --platform linux/amd64,linux/arm64 -t DOCKER_USERNAME/db-migration:${imageTag} -f Dockerfile.db-migration --push .'
+				        sh 'docker logout'
               }
             }
           }
